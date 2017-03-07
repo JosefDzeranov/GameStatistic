4234604c5364784c9cecc17ffeea427f8789067e
Josef Dzeranov <soslan29@gmail.com>
2017-03-07 10:18:46 +0300

    Add models Match and Player

---
+++ Kontur.GameStats-master/Kontur.GameStats.Server/Models/Match.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontur.GameStats.Server.Models
+{
+    class Match
+    {
+        public string Id { get; set; }
+        public string Map { get; set; }
+        public string GameMode { get; set; }
+        public int FragLimit { get; set; }
+        public int TimeLimit { get; set; }
+        public double TimeElapsed { get; set; }
+        public Player[] Scoreboard { get; set; }
+        public string Timestamp { get; set; }
+        
+    }
+}
---
+++ Kontur.GameStats-master/Kontur.GameStats.Server/Models/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Kontur.GameStats.Server.Models
+{
+    public class Player
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Frags { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+
+    }
+}
