4b1023ba99fe378064fc6eaddde5960b9f0c89d9
Josef Dzeranov <soslan29@gmail.com>
2017-03-07 10:07:38 +0300

    Add GameServerModel

---
+++ Kontur.GameStats-master/Kontur.GameStats.Server/Models/GameServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontur.GameStats.Server.Models
+{
+    class GameServer
+    {
+        public string EndPoint { get; set; }
+        public string Name { get; set; }
+        public string[] GameModes { get; set; }
+    }
+}
