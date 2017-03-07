6cfaae2e0d28a6ddb32f75e0e28b7943bc86f9b3
Josef Dzeranov <soslan29@gmail.com>
2017-03-07 10:04:30 +0300

    Add kontur shablon project

---
+++ KonturBackEnd/src/KonturBackEnd/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KonturBackEnd.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string GameModes { get; set; }
+
+    }
+}
